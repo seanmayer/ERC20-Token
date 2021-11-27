f92f0e423d8f13285ff91380a2c1711acac4a31d
Sean Mayer <2008sean@live.com>
2021-11-27 09:31:15 +0000

    Added ICO contract functionality
    - ICO state variables and constructor
    - Implemented invest() function

--- contracts/erc20_state_vars.sol
+++ contracts/erc20_state_vars.sol
@@ -22,7 +22,7 @@ interface ERC20Interface {
     
 }
 
-contract Cryptos is ERC20Interface {
+contract TestToken is ERC20Interface {
     string public name = "Test";
     string public symbol = "TST";
     uint public decimals = 0; //18
@@ -80,6 +80,82 @@ contract Cryptos is ERC20Interface {
         return true;
         
     }
+}
 
+contract TestTokenICO is TestToken {
+    address public admin;
+    address payable public deposit;
+    uint tokenPrice = 0.001 ether;
+    uint public hardCap = 300 ether;
+    uint public raisedAmount;
+    uint public saleStart = block.timestamp; //+ 3600; <- start in one hour after deployment
+    uint public saleEnd = block.timestamp + 604800; // ICO ends in one week
+    uint public tokenTradeStart = saleEnd + 604800; // transferable a week after sale ends
+    uint public maxInvestment = 5 ether;
+    uint public minInvestment = 0.1 ether;
+
+    enum State {beforeStart, running, afterEnd, halted}
+    State public icoState;
+
+    constructor(address payable _deposit) {
+        deposit = _deposit;
+        admin = msg.sender;
+        icoState = State.beforeStart;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
     
+    function halt() public onlyAdmin {
+        icoState = State.halted;
+    }
+
+    function resume() public onlyAdmin {
+        icoState = State.running;
+    }
+
+    function changeDepositAddress(address payable newDeposit) public onlyAdmin {
+        deposit = newDeposit;
+    }
+
+    function getCurrentState() public view returns(State) {
+        if(icoState == State.halted) { 
+            return State.halted; 
+            } else if (block.timestamp < saleEnd) {
+                return State.beforeStart;
+            } else if (block.timestamp >= saleStart &&  block.timestamp <= saleEnd) {
+                return State.running;
+            } else {
+                return State.afterEnd;
+            }
+    }
+
+    event Invest(address investor, uint value, uint tokens);
+
+    function invest() payable public returns(bool) {
+        icoState = getCurrentState();
+        require(icoState == State.running);
+
+        require(msg.value >= minInvestment && msg.value <= maxInvestment);
+        raisedAmount += msg.value;
+        require(raisedAmount <= hardCap);
+
+        uint tokens = msg.value / tokenPrice;
+
+        balances[msg.sender] += tokens;
+        balances[founder] -= tokens;
+        deposit.transfer(msg.value);
+        emit Invest(msg.sender, msg.value, tokens);
+
+        return true;
+
+    }
+
+    receive() payable external {
+        invest();
+    }
+
+
 }
